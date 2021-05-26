0e3f23bf994e16374c383f0d2679743a00ea90fa
42seouler <42seouler@gmail.com>
2021-05-26 21:44:23 +0900

    spring project 생성
    프로젝트 시작을 위한 프로젝트 생성
    
    dependencies
    spring web
    spring JPA
    Lombok
    h2 database
    mysql connector

---
+++ zenly/src/main/java/com/sparta/zenly/ZenlyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.zenly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenlyApplication.class, args);
+	}
+
+}
---
+++ zenly/src/test/java/com/sparta/zenly/ZenlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.zenly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
